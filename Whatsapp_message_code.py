0a3ed72c6f484d14d2ef9e1aaf469b11fb6ee84e
sai <saik62907@gmail.com>
2025-03-03 17:35:18 +0530

    Done

--- Whatsapp_message_code.py
+++ Whatsapp_message_code.py
@@ -7,9 +7,10 @@ x = 10
 phone_number = "+919876543251"
 message = "Hello Git"
 webbrowser.open(f"https://web.whatsapp.com/send?phone={+919876543251}")
+count = x
 time.sleep(1)
 
-for i in range (x):
+for i in range (count):
     pyperclip.copy(message)
     pyautogui.hotkey("ctrl", "v")
     pyautogui.press("enter")
